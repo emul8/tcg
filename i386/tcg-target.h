1ba05dec3a8719ee723d321e772edadf2c3645cc
Mateusz Hołenko <mholenko@antmicro.com>
2015-11-25 16:39:03 +0100

    [#1] Change register storing cpu state from `ebp` to `ebx`.
    This is to workaround a bug of some gcc versions that
    ignore `-fomit-frame-pointer` and use `ebp` register
    even if it is explicitly forbidden by a programmer.

--- i386/tcg-target.h
+++ i386/tcg-target.h
@@ -122,7 +122,7 @@ typedef enum {
 #if TCG_TARGET_REG_BITS == 64
 # define TCG_AREG0 TCG_REG_R14
 #else
-# define TCG_AREG0 TCG_REG_EBP
+# define TCG_AREG0 TCG_REG_EBX
 #endif
 
 static inline void flush_icache_range(unsigned long start, unsigned long stop)
